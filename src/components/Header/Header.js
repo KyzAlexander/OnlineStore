bd6df361ccb00db388c4575f39cd76dd43ac4184
K308-4\k3084 <54>
2022-04-26 17:15:15 +0300

    add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import { Basket, Like, User } from "../../assets/images/Icons";
+import logo from "../../assets/images/logo.png";
+import "./header.scss";
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <div className='header__headerLeft'>
+        <img src={logo} alt='logo'></img>
+        <div className='headerLeft__info'>
+          <h1>REACT SNEAKERS</h1>
+          <p>Магазин лучших кроссовок</p>
+        </div>
+      </div>
+      <ul className='header__headerRight'>
+        <li>
+          <Basket />
+          <span>1250 руб.</span>
+        </li>
+        <li>
+          <Like />
+        </li>
+        <li>
+          <User />
+        </li>
+      </ul>
+    </header>
+  );
+}
