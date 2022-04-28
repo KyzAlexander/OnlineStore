e6f70ea680ef283524bf90bbe8725d3fed94207b
K308-4\k3084 <54>
2022-04-28 16:27:16 +0300

    add scss listCard

--- src/components/ListCards/ListCards.js
+++ src/components/ListCards/ListCards.js
@@ -1,13 +1,25 @@
 import Card from "../Card/Card";
+import "./listCard.scss";
 
 export default function ListCards() {
   return (
     <div className='ListCards'>
       <div className='wrapperTitleAndSearch'>
         <h2 className='wrapperTitleAndSearch__title'>Все кроссовки</h2>
-        <input className='wrapperTitleAndSearch__search'></input>
+        <input
+          className='wrapperTitleAndSearch__search'
+          type='search'
+          placeholder='Поиск...'
+        ></input>
       </div>
       <div className='list'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
       </div>
     </div>
